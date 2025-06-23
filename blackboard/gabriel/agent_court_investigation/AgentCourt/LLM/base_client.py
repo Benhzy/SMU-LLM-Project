a54eb27e8fcc5a5af5ba36abb7a50df8c0a915ce
@zhiyi_hao1 <hao_zhiyi@agc.gov.sg>
2025-06-23 08:22:15 +0800

    Code Cleanup
    Removed blackboard and cleaned up requirements.txt

--- blackboard/gabriel/agent_court_investigation/AgentCourt/LLM/base_client.py
+++
@@ -1,9 +0,0 @@
-# LLM/base_client.py
-from abc import ABC, abstractmethod
-from typing import List, Dict
-
-
-class BaseClient(ABC):
-    @abstractmethod
-    def send_request(self, messages: List[Dict[str, str]], *args, **kwargs):
-        pass
